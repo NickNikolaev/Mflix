7ea76c726ef7b97f2463508238464bb516bdebfb
Nikola Nikolaev <69590152+NikolaNikolaev@users.noreply.github.com>
2022-10-01 15:52:13 +0300

    Change PORT

--- spa/src/config/config.js
+++ spa/src/config/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'http://localhost:4200';
 export const AUTH_TOKEN = 'auth_token';
 export const SECRET_KEY = 'secretsecret';
